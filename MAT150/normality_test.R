5f402092c0cb231235ac8463d55ad1ce40a6d70e
Ben England <benengland82@gmail.com>
2023-03-24 14:56:20 -0400

    radically simplify the code
    use vectorization features of R

--- MAT150/normality_test.R
+++ MAT150/normality_test.R
@@ -1,10 +1,5 @@
 # function to compute median normal z-scores for a normality test
 # the probability for each score is 
 normal_zscores <- function(sample_size) {
-  normal_zscore_list = vector(mode="numeric", length=sample_size)
-  for (i in 1:sample_size) {
-    next_probability = (2*i - 1) / (2*sample_size)
-    normal_zscore_list[i] = qnorm(next_probability)
-  }
-  normal_zscore_list
+  qnorm( (seq(1, sample_size)*2 - 1) / (2*sample_size))
 }
